9022bed57d7b3b19b863049d31d53af98cb1bbe3
mohammadp1001 <mohammad.p69@gmail.com>
2021-04-19 00:25:05 +0200

    Complete svm.ipynb

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -34,7 +34,7 @@ class LinearClassifier(object):
 
     # Run stochastic gradient descent to optimize W
     loss_history = []
-    for it in xrange(num_iters):
+    for it in range(num_iters):
       X_batch = None
       y_batch = None
 
@@ -43,27 +43,29 @@ class LinearClassifier(object):
       # Sample batch_size elements from the training data and their           #
       # corresponding labels to use in this round of gradient descent.        #
       # Store the data in X_batch and their corresponding labels in           #
-      # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
+      # y_batch; after sampling X_batch should have shape (batch_size,dim)    #
       # and y_batch should have shape (batch_size,)                           #
       #                                                                       #
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      batch_indic = np.random.choice(np.arange(num_train), batch_size, replace=True) #solution
+      X_batch = X[batch_indic,:] #solution
+      y_batch = y[batch_indic]  #solution
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
 
       # evaluate loss and gradient
       loss, grad = self.loss(X_batch, y_batch, reg)
-      loss_history.append(loss)
+      loss_history.append(loss) 
 
       # perform parameter update
       #########################################################################
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W += -grad*learning_rate #solution
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -91,7 +93,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    y_pred = np.argmax(np.dot(X,self.W),axis=1) #solution
+
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
